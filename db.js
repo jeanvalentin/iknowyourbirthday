7fc8ac434b8a0961709819f27618490979aa13f2
Jean Valentin <jeanvalentin@users.noreply.github.com>
2022-10-29 23:53:37 +0200

    Create prisma dumper

---
+++ db.js
@@ -0,0 +1,29 @@
+const { PrismaClient } = require('@prisma/client');
+const { writeFileSync } = require('fs');
+
+const prisma = new PrismaClient()
+
+async function main() {
+  // quick update
+  // await prisma.birthday.updateMany({ where: { name: 'Bobby' }, data: { year: 1984 } });
+
+  const dump = await prisma.birthday.findMany({
+    select: {
+      day: true,
+      month: true,
+      year: true,
+      name: true,
+    },
+  });
+  writeFileSync('birthdays.json', JSON.stringify(dump));
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
