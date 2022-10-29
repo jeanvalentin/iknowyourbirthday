c9ea42dba0989706e0ca4d2002eb0e0c1013fa42
Jean Valentin <jeanvalentin@users.noreply.github.com>
2022-10-30 00:00:41 +0200

    Create main page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
-import Head from 'next/head'
+import { Container, Text, Title } from '@mantine/core';
+import dayjs from 'dayjs';
+import Head from 'next/head';
+import birthdays from '../birthdays.json';
 
 export default function Home() {
-  return <>
+  const now = dayjs();
+  const birthdaysToday = birthdays.filter(v => now.format('D') == v.day && now.format('M') == v.month);
+  return <Container>
     <Head>
       <title>Birthdays!</title>
     </Head>
-    hello
-  </>
+    <Title>{now.format('D MMMM YYYY')}</Title>
+    {birthdaysToday.map((v, k) => {
+      return <Text key={k}>
+        {v.name}
+        {v.year && <>: {now.get('y') - v.year} ({v.year})</>}
+      </Text>
+    })}
+    <Text></Text>
+  </Container>
 }
